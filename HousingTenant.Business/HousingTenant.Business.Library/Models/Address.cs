ded553064c12f9c59a3ede696238a70bb9bf6717
Daniel Larner <daniel.larner@outlook.com>
2017-07-13 10:49:36 -0400

    add address model to business logic project [#148767851]

---
+++ HousingTenant.Business/HousingTenant.Business.Library/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingTenant.Business.Library.Models
+{
+  public class Address
+  {
+    public string Line1 { get; set; }
+    public string Line2 { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public int ZipCode { get; set; }
+  }
+}
