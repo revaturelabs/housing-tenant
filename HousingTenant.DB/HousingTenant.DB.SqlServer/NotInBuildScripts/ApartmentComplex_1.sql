91a03216958db25e39b297148a13f189cfba738a
Samuel Oates <samuel.oates1@gmail.com>
2017-07-13 15:22:25 -0400

    Added apartmentcomplex table to apartment schema. [#148838061]

---
+++ HousingTenant.DB/HousingTenant.DB.SqlServer/NotInBuildScripts/ApartmentComplex_1.sql
@@ -0,0 +1,8 @@
+﻿create table Apartment.ApartmentComplex
+(
+  ApartmentComplexID int not null primary key identity
+  ,AddressID int not null foreign key references Address.Address(AddressID)
+  ,WalkingDistance bit not null
+  ,ComplexName nvarchar(50) not null
+  ,Active bit not null default(1) 
+)
