0ce112847a3edab1a174bfe0252dbf087356075e
Samuel Oates <samuel.oates1@gmail.com>
2017-07-13 15:53:21 -0400

    Added supply table. [#148839151]

---
+++ HousingTenant.DB/HousingTenant.DB.SqlServer/NotInBuildScripts/Supply.sql
@@ -0,0 +1,10 @@
+﻿create table Request.Supplies
+(
+  RequestID int not null primary key foreign key references Request.Request(RequestID)
+  ,Soap bit not null
+  ,PaperTowels bit not null
+  ,TrashBags bit not null
+  ,Sponges bit not null
+  ,Content nvarchar(500)
+  ,Active bit not null default(1)
+)
