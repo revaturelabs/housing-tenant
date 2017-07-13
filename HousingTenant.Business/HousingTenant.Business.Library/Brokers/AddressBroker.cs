f9e628055111d02ea5cb001951341d2631f7e287
Daniel Larner <daniel.larner@outlook.com>
2017-07-13 14:48:23 -0400

    delete get method in address broker class [#148837661]

--- HousingTenant.Business/HousingTenant.Business.Library/Brokers/AddressBroker.cs
+++ HousingTenant.Business/HousingTenant.Business.Library/Brokers/AddressBroker.cs
@@ -10,20 +10,6 @@ namespace HousingTenant.Business.Library.Brokers
 {
   public class AddressBroker
   {
-    public async Task<Address> GetAddress()
-    {
-      var client = new HttpClient();
-
-      var address = new Address();
-
-      var response = await client.GetAsync("URI/address", HttpCompletionOption.ResponseContentRead);
-
-      if (response.IsSuccessStatusCode)
-      {
-        address = JsonConvert.DeserializeObject<Address>(response.Content.ReadAsStringAsync().Result);
-        return address;
-      }
-      return null;
-    }
+    
   }
 }
