3d818a34988548c639f6d51bcda5d75f4d7415e2
Grant Taylor <grant.devlin.taylor@gmail.com>
2017-07-13 12:00:45 -0400

    finish Added Nunit package and NunitTestTracker package. Added Nunit test for GetAddress(). [#148832731]

--- HousingTenant.Data/HousingTenant.Data.Library/DataBroker.cs
+++ HousingTenant.Data/HousingTenant.Data.Library/DataBroker.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+
+
+
 using HousingTenant.Data.Library.DataModels;
 
 namespace HousingTenant.Data.Library
 {
     public class DataBroker
     {
+        
         public Address GetAddressByID(int addressID)
         {
             // Database, give me a thing
-            Address address = new DataModels.Address();
-
+            Address address = new DataModels.Address()
+            {
+                Address1 = "1234 Nowhere St.",
+                Address2 = "Apt, 5",
+                City = "Reston",
+                State = "VA",
+                Zip = "18555"
+            };
 
             return address;
         }
---
+++ HousingTenant.Data/HousingTenant.Data.Tests/DataBrokerAddressTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HousingTenant.Data.Library;
+using HousingTenant.Data.Library.DataModels;
+using NUnit.Framework;
+
+namespace HousingTenant.Data.Tests
+{
+    [TestFixture]
+    public class DataBrokerAddressTests
+    {
+        [Test]
+        void GetAddressTest()
+        {
+            DataBroker broker = new DataBroker();
+            Address test = broker.GetAddressByID(1);
+
+            Assert.NotNull(test);
+            Assert.NotNull(test.Address1);
+            Assert.NotNull(test.City);
+            Assert.NotNull(test.State);
+            Assert.NotNull(test.Zip);
+        }
+
+    }
+}
