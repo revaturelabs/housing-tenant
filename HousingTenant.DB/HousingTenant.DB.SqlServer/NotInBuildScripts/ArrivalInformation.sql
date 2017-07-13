870bb391b18efe02239c45e44dc86618cb0302b5
Samuel Oates <samuel.oates1@gmail.com>
2017-07-13 17:03:10 -0400

    Added arrivalinformation table to arrival schema. [#148839709]

---
+++ HousingTenant.DB/HousingTenant.DB.SqlServer/NotInBuildScripts/ArrivalInformation.sql
@@ -0,0 +1,9 @@
+﻿create table Arrival.ArrivalInformation
+(
+  ArrivalInformationID int not null primary key identity
+  ,BatchID int not null foreign key references Batch.Batch(BatchID)
+  ,PersonID int not null foreign key references Person.Person(PersonID)
+  ,ArrivalDate date not null
+  ,ArrivalTime time
+  ,HasCar bit not null
+)
