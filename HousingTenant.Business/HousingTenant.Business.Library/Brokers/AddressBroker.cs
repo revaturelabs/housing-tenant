a0b9f7ddc654be0fc47d636a2818f8f8c781b1e0
Daniel Larner <daniel.larner@outlook.com>
2017-07-13 13:44:53 -0400

    add async method to address broker [#148837661]

--- HousingTenant.Business/HousingTenant.Business.Library/Brokers/AddressBroker.cs
+++ HousingTenant.Business/HousingTenant.Business.Library/Brokers/AddressBroker.cs
@@ -1,15 +1,29 @@
 ﻿using HousingTenant.Business.Library.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace HousingTenant.Business.Library.Brokers
 {
   public class AddressBroker
   {
-    public Address GetAddress()
+    public async Task<Address> GetAddress()
     {
-      throw new NotImplementedException(); //jon's api
+      var client = new HttpClient();
+
+      var address = new Address();
+
+      var response = await client.GetAsync("URI/address", HttpCompletionOption.ResponseContentRead);
+
+      if (response.IsSuccessStatusCode)
+      {
+        address = JsonConvert.DeserializeObject<Address>(response.Content.ReadAsStringAsync().Result);
+        return address;
+      }
+      return null;
     }
   }
 }
