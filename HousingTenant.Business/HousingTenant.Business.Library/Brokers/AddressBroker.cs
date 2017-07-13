b7b8dc5ecde57f5a81b66a49d4179d15d6ed9190
Daniel Larner <daniel.larner@outlook.com>
2017-07-13 13:27:26 -0400

    add get method to address broker class [#148837661]

---
+++ HousingTenant.Business/HousingTenant.Business.Library/Brokers/AddressBroker.cs
@@ -0,0 +1,15 @@
+﻿using HousingTenant.Business.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingTenant.Business.Library.Brokers
+{
+  public class AddressBroker
+  {
+    public Address GetAddress()
+    {
+      throw new NotImplementedException(); //jon's api
+    }
+  }
+}
