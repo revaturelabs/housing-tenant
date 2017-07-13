bf658dc19fccb32fde92d103b0f706be6c2bf191
Grant Taylor <grant.devlin.taylor@gmail.com>
2017-07-13 10:39:45 -0400

    start created Address model, Created DataBroker class with GetAddressByID stub.  [#148832731]

---
+++ HousingTenant.Data/HousingTenant.Data.Library/DataBroker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HousingTenant.Data.Library.DataModels;
+
+namespace HousingTenant.Data.Library
+{
+    public class DataBroker
+    {
+        public Address GetAddressByID(int addressID)
+        {
+            // Database, give me a thing
+            Address address = new DataModels.Address();
+
+
+            return address;
+        }
+    }
+}
