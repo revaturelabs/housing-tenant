e1356e6d97f6250bf18a4b6a907a80be4ba0efd8
Grant Taylor <grant.devlin.taylor@gmail.com>
2017-07-14 13:24:13 -0400

    finished adding MaintenanceRequest to library. [finishes #148907817]

---
+++ HousingTenant.Data/HousingTenant.Data.Tests/ModelTests/MaintenanceRequestTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using HousingTenant.Data.Library.DataModels;
+
+namespace HousingTenant.Data.Tests.ModelTests
+{
+    [TestFixture]
+    public class MaintenanceRequestTest
+    {
+        [Test]
+        public void CreateMaintenanceRequest()
+        {
+            var test = new MaintenanceRequest();
+
+            Assert.IsNotNull(test);
+        }
+    }
+}
