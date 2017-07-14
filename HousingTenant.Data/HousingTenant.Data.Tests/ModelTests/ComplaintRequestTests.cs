afc8b457572b3e77008035c41f4625212c652a74
Grant Taylor <grant.devlin.taylor@gmail.com>
2017-07-14 11:25:33 -0400

    delivered ComplaintRequest; object created just fine.  [delivers #148908167]

--- HousingTenant.Data/HousingTenant.Data.Tests/ModelTests/ComplaintRequestTests.cs
+++ HousingTenant.Data/HousingTenant.Data.Tests/ModelTests/ComplaintRequestTests.cs
@@ -2,11 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
+using HousingTenant.Data.Library.DataModels;
 
 namespace HousingTenant.Data.Tests.ModelTests
 {
+    [TestFixture]
     public class ComplaintRequestTests
     {
+        [Test]
+        public void CreateComplaintRequest()
+        {
+            ComplaintRequest test = new ComplaintRequest();
 
+            Assert.IsNotNull(test);
+        }
     }
 }
