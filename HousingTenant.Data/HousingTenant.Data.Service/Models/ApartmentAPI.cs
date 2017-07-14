ea21b6af813b9d1c353fbd5e541f0f94a673d649
Jonathan <jonvette@comcast.net>
2017-07-14 14:57:38 -0400

    Added Apartment Model [#148927021]

---
+++ HousingTenant.Data/HousingTenant.Data.Service/Models/ApartmentAPI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingTenant.Data.Service.Models
+{
+  public class ApartmentAPI
+    {
+      public AddressAPI Address { get; set; }
+      public bool WalkingDistance { get; set; }
+      public int NumBeds { get; set; }
+      public Gender AGender { get; set; }
+      public string ComplexName { get; set; }
+      public int NumCars { get; set; }
+    }
+}
