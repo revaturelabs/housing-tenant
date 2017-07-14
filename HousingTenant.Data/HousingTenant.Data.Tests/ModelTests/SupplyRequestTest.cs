953e9f5eab0f1bed161c0528ba6c1d8d279f7417
Grant Taylor <grant.devlin.taylor@gmail.com>
2017-07-14 11:48:07 -0400

    finished Adding SupplyRequest to DataModels [finishes #148907995]

---
+++ HousingTenant.Data/HousingTenant.Data.Tests/ModelTests/SupplyRequestTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using HousingTenant.Data.Library.DataModels;
+
+namespace HousingTenant.Data.Tests.ModelTests
+{
+    [TestFixture]
+    public class SupplyRequestTest
+    {
+        [Test]
+        public void CreateSupplyRequest()
+        {
+            var test = new SupplyRequest();
+
+            Assert.IsNotNull(test);
+
+            Assert.IsTrue(test.supplies.Count == (int)Supplies.numSupplies);
+        }
+    }
+}
