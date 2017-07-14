43ccd9624f9510056c0527c71f24e6019cf5723c
Daniel Larner <daniel.larner@outlook.com>
2017-07-14 10:23:47 -0400

    add properties to person model class [#148767293]

--- HousingTenant.Business/HousingTenant.Business.Library/Models/Person.cs
+++ HousingTenant.Business/HousingTenant.Business.Library/Models/Person.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace HousingTenant.Business.Library.Models
 {
-    class Person
-    {
-    }
+  public enum Gender { male, female }
+
+  public class Person
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Gender Gender { get; set; }
+    public string PhoneNumber { get; set; }
+    public List<Address> Address { get; set; }
+    public string Apartment { get; set; }
+    public DateTime Arrival { get; set; }
+    public string BatchName { get; set; }
+    //public List<Request> Request { get; set; }
+    public bool HasCar { get; set; }
+  }
 }
