6d61a7672a2f50fd24656e0c356f65fb1aa8ecef
Grant Taylor <grant.devlin.taylor@gmail.com>
2017-07-14 10:39:27 -0400

    start added person.cs to datamodels folder [#148906309]

---
+++ HousingTenant.Data/HousingTenant.Data.Library/DataModels/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingTenant.Data.Library.DataModels
+{
+    public enum Gender { male, female };
+    public class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public Gender gender { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+    }
+}
