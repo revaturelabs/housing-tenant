3fc151c73fd97c081b7d6fb8de18b9d023655d01
Grant Taylor <grant.devlin.taylor@gmail.com>
2017-07-14 11:15:34 -0400

    finished adding properties to ComplaintRequest [finishes #148908167]

--- HousingTenant.Data/HousingTenant.Data.Library/DataModels/ComplaintRequest.cs
+++ HousingTenant.Data/HousingTenant.Data.Library/DataModels/ComplaintRequest.cs
@@ -7,8 +7,11 @@ namespace HousingTenant.Data.Library.DataModels
     public class ComplaintRequest
     {
         public string content { get; set; }
+
         public string firstName { get; set; }
+
         public string lastName { get; set; }
+
         public DateTime timeOfRequest { get; set; }
     }
 }
