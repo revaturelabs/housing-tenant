23f62d4c74ba1cacb3aa993dd25cf862f2e2f277
Grant Taylor <grant.devlin.taylor@gmail.com>
2017-07-14 13:45:21 -0400

    finished creating Apartment class. had to re-add Address class that got lost during the entity framework setup. [finishes #148907755]

--- HousingTenant.Data/HousingTenant.Data.Library/DataModels/Apartment.cs
+++ HousingTenant.Data/HousingTenant.Data.Library/DataModels/Apartment.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using HousingTenant.Data.Library.DataModels;
 
 namespace HousingTenant.Data.Library.DataModels
 {
     public class Apartment
     {
-
+        public Address address { get; set; }
+        public bool walkingDist { get; set; }
+        public int numBeds { get; set; }
+        public string gender { get; set; }
+        public string complexString { get; set; }
+        public int numCars { get; set; }
     }
 }
