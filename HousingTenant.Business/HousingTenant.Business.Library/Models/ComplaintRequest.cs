75b82be95410b66acc58d46e218a22143fcc36e9
Daniel Larner <daniel.larner@outlook.com>
2017-07-16 17:50:59 -0400

    add complaint request model class to business logic project [#148767839]

---
+++ HousingTenant.Business/HousingTenant.Business.Library/Models/ComplaintRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingTenant.Business.Library.Models
+{
+  public class ComplaintRequest
+  {
+    public string Content { get; set; }
+    public bool Status { get; set; }
+  }
+}
