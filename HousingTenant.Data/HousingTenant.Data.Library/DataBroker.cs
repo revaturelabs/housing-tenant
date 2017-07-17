71e082db827eed7572b377e22eff573d8e395c0e
Samuel Oates <samuel.oates1@gmail.com>
2017-07-17 11:26:46 -0400

    Get all Addresses feature complete. [#148925429]

--- HousingTenant.Data/HousingTenant.Data.Library/DataBroker.cs
+++ HousingTenant.Data/HousingTenant.Data.Library/DataBroker.cs
@@ -5,22 +5,33 @@ using HousingTenant.Data.Library.Models;
 
 namespace HousingTenant.Data.Library
 {
-    public class DataBroker
-    {
-        
-        public Address GetAddressByID(int addressID)
-        {
-            // Database, give me a thing
-            Address address = new Address()
-            {
-                Address1 = "1234 Nowhere St.",
-                Address2 = "Apt, 5",
-                City = "Reston",
-                State = "VA",
-                Zip = 18555
-            };
+  public class DataBroker
+  {
 
-            return address;
-        }
+    public Address GetAddressByID(int addressID)
+    {
+      TenantDBContext ctx = new TenantDBContext();
+      // Database, give me a thing
+      Address address = new Address()
+      {
+        Address1 = "1234 Nowhere St.",
+        Address2 = "Apt, 5",
+        City = "Reston",
+        State = "VA",
+        Zip = 18555
+      };
+
+      return address;
     }
+    public List<Address> GetAddresses()
+    {
+      TenantDBContext ctx = new TenantDBContext();
+      var addresses = new List<Address>();
+      foreach(var item in ctx.Address)
+      {
+        addresses.Add(item);
+      }
+      return addresses;
+    }
+  }
 }
