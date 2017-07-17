d704f50d0ed35550179ad62c950d80576063178d
Lucas Carver <lucas.carver@outlook.com>
2017-07-17 15:11:13 -0400

    Deleted js files from main directory, since these files will now be building in the client directory [#149028655]

--- js/home/module.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ng = require("angular");
-require("angular-material");
-var home = ng.module('ngHome', ['ngMaterial']);
-exports.home = home;
