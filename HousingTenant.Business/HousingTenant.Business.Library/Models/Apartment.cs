e4e1d872d876c35271b42a8ca0e96d3d7bb68209
Daniel Larner <daniel.larner@outlook.com>
2017-07-17 11:03:43 -0400

    add test class for apartment model class [finishes #148767849]

--- HousingTenant.Business/HousingTenant.Business.Library/Models/Apartment.cs
+++ HousingTenant.Business/HousingTenant.Business.Library/Models/Apartment.cs
@@ -9,7 +9,7 @@ namespace HousingTenant.Business.Library.Models
     public string ComplexName { get; set; }
     public Address Address { get; set; }
     public int NumBeds { get; set; }
-    public string Gender { get; set; }
+    public Gender Gender { get; set; }
     public bool WalkingDistance { get; set; }
   }
 }
