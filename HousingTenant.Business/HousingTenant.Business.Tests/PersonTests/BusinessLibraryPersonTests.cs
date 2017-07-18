8e65c3ca18232f27d3dd563ad91a637f611ffa10
omar reid <omar.reid@verizon.net>
2017-07-18 11:07:53 -0400

    Tested Person class using PersonEqualsPositiveTest, PersonEqualsNegativeTest, PersonGetFullNameTest, PersonGetHashCodeTest, and PersonToStringTest. All test passed. Work done on Person class. [delivers #149067519]

---
+++ HousingTenant.Business/HousingTenant.Business.Tests/PersonTests/BusinessLibraryPersonTests.cs
@@ -0,0 +1,87 @@
+﻿using HousingTenant.Business.Library.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingTenant.Business.Tests.PersonTests
+{
+    [TestFixture]
+    public class BusinessLibraryPersonTests
+    {
+        [Test]
+        public void PersonEqualsPositiveTest()
+        {
+            var person1 = new Person
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+            var person2 = new Person
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            Assert.IsTrue(person1.Equals(person2));
+        }
+
+        [Test]
+        public void PersonEqualsNegativeTest()
+        {
+            var person1 = new Person
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+            var person2 = new Person
+            {
+                FirstName = "John",
+                LastName = "Dae"
+            };
+
+            Assert.IsFalse(person1.Equals(person2));
+        }
+
+        [Test]
+        public void PersonGetFullNameTest()
+        {
+            var person1 = new Person
+            {
+               FirstName = "John",
+               LastName = "Doe"
+            };
+
+            var actual = person1.GetFullName();
+            var expected = "John Doe";
+
+            Assert.IsTrue(actual.Equals(expected));
+        }
+
+        [Test]
+        public void PersonGetHashCodeTest()
+        {
+            var person1 = new Person
+            {
+               FirstName = "John",
+               LastName = "Doe"
+            };
+            var actual = person1.GetHashCode();
+
+            Assert.IsTrue(actual > 0);
+        }
+        
+        [Test]
+        public void PersonToStringTest()
+        {
+            var person1 = new Person
+            {
+                FirstName = "John",
+                LastName = "Doe"
+            };
+            var actual = person1.ToString();
+
+            Assert.IsNotNull(actual);
+        }
+    }
+}
