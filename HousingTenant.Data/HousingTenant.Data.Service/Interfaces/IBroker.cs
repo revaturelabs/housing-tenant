fe7816986103d37051d0606593111b31dbb95291
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-18 13:10:55 -0400

    created RequestBroker class [#149086383]

--- HousingTenant.Data/HousingTenant.Data.Service/Interfaces/IBroker.cs
+++ HousingTenant.Data/HousingTenant.Data.Service/Interfaces/IBroker.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace HousingTenant.Data.Service.Interface
+namespace HousingTenant.Data.Service.Interfaces
 {
     public interface IBroker<T>
     {
