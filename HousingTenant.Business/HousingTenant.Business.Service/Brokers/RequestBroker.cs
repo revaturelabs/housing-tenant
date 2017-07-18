71b3792369cc9e96f7f8a328a53f845a8d41c0aa
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-18 16:31:45 -0400

    Added description to IBroker and RequestBroker inherirts IBroker where all functions use the correct model. [#149112511]

--- HousingTenant.Business/HousingTenant.Business.Service/Brokers/RequestBroker.cs
+++ HousingTenant.Business/HousingTenant.Business.Service/Brokers/RequestBroker.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HousingTenant.Business.Service.Interfaces;
+using HousingTenant.Business.Library.Models;
 
 namespace HousingTenant.Business.Service.Brokers
 {
-    public class RequestBroker
+    public class RequestBroker : IBroker<ARequest>
     {
+        public bool Create(ARequest obj)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public bool Delete(ARequest obj)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public List<ARequest> Get()
+        {
+            throw new NotImplementedException ();
+        }
+
+        public bool Update(ARequest obj)
+        {
+            throw new NotImplementedException ();
+        }
     }
 }
--- HousingTenant.Business/HousingTenant.Business.Service/Interfaces/IBroker.cs
+++ HousingTenant.Business/HousingTenant.Business.Service/Interfaces/IBroker.cs
@@ -5,8 +5,12 @@ using System.Threading.Tasks;
 
 namespace HousingTenant.Business.Service.Interfaces
 {
+    /// <summary>
+    /// The base interface for all Brokers used by the Business Service
+    /// </summary>
     public interface IBroker<T>
     {
+        
         List<T> Get();
         bool Create(T obj);
         bool Delete(T obj);
