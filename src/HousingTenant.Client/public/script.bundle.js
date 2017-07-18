992bc5114ac10442fd7493ecdf237f57312f67de
Julian Rojas <rojasmolinajulian@gmail.com>
2017-07-18 13:46:53 -0400

    Functioning request controller. Populates a list of request and allows for new requests to be added [finishes #149083763]

--- src/HousingTenant.Client/public/script.bundle.js
+++ src/HousingTenant.Client/public/script.bundle.js
@@ -166,8 +166,8 @@ module_1.home.controller('homeController', ['$scope', 'homeFactory', function ($
 Object.defineProperty(exports, "__esModule", { value: true });
 var module_1 = __webpack_require__(15);
 var Request = (function () {
-    function Request(n) {
-        this.Name = n;
+    function Request() {
+        this.Name = " ";
         this.Soap = true;
         this.ToiletPaper = false;
         this.PaperTowels = false;
@@ -179,9 +179,43 @@ var Request = (function () {
     return Request;
 }());
 var supplyController = module_1.supplyModule.controller('suppliesCtrl', ['$scope', function ($scope) {
-        $scope.requestList = [
-            new Request('First Request'), new Request('Second Request'), new Request('Third Request')
-        ];
+        var requestModal = document.getElementById('AddRequestModal');
+        var req1 = new Request;
+        req1.Name = 'First Request';
+        var req2 = new Request;
+        req2.Name = 'Second Request';
+        var req3 = new Request;
+        req3.Name = 'Third Request';
+        $scope.requestList = [];
+        $scope.requestList.push(req1);
+        $scope.requestList.push(req2);
+        $scope.requestList.push(req3);
+        $scope.addRequest = function (n, s, tp, pt, ds, tb, dd, sp) {
+            var request = {
+                Name: n,
+                Soap: s,
+                ToiletPaper: tp,
+                PaperTowels: pt,
+                DishSoap: ds,
+                TrashBags: tb,
+                DishwasherDetergent: dd,
+                Sponges: sp
+            };
+            $scope.requestList.push(request);
+            $scope.closeModal();
+            console.log($scope.requestList);
+        };
+        $scope.openModal = function () {
+            requestModal.style.display = 'block';
+        };
+        $scope.closeModal = function () {
+            requestModal.style.display = 'none';
+        };
+        window.onclick = function (event) {
+            if (event.target == requestModal) {
+                requestModal.style.display = 'none';
+            }
+        };
     }]);
 
 
--- src/HousingTenant.Client/ts/supplies/controllers.ts
+++ src/HousingTenant.Client/ts/supplies/controllers.ts
@@ -10,8 +10,8 @@ class Request {
    DishwasherDetergent : boolean;
    Sponges : boolean;
 
-   constructor(n:string){
-      this.Name = n;   
+   constructor(){
+      this.Name = " ";   
       this.Soap = true;
       this.ToiletPaper = false;
       this.PaperTowels = false;
@@ -24,7 +24,50 @@ class Request {
 
 
 var supplyController = sm.controller('suppliesCtrl', ['$scope', function($scope){
-   $scope.requestList = [
-      new Request('First Request'), new Request('Second Request'), new Request('Third Request')
-   ]; 
+   
+   var requestModal = document.getElementById('AddRequestModal');
+   
+   var req1 = new Request;
+   req1.Name = 'First Request';
+   var req2 = new Request;
+   req2.Name = 'Second Request';
+   var req3 = new Request;
+   req3.Name = 'Third Request';
+
+   $scope.requestList = [];
+   $scope.requestList.push(req1);
+   $scope.requestList.push(req2);
+   $scope.requestList.push(req3);
+
+   $scope.addRequest = function(n,s,tp,pt,ds,tb,dd,sp){
+      var request = {
+            Name: n,
+            Soap: s,
+            ToiletPaper : tp,
+            PaperTowels : pt,
+            DishSoap : ds,
+            TrashBags : tb,
+            DishwasherDetergent : dd,
+            Sponges : sp
+      }
+      $scope.requestList.push(request);
+      $scope.closeModal();
+
+      console.log($scope.requestList);
+   }
+
+   $scope.openModal = function(){
+      requestModal.style.display = 'block';
+   }
+
+   $scope.closeModal = function(){
+      requestModal.style.display = 'none';
+   }
+   window.onclick = function(event){
+      if(event.target == requestModal){
+         requestModal.style.display = 'none';
+      }
+   }
 }]);
+
+
