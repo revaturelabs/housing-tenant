cc57eaab45c59b7777b718bacfd98e1d324b6faf
omar reid <omar.reid@verizon.net>
2017-07-18 13:10:10 -0400

    Created ARequest class that will serve as the base class for all request that can be submitted by a tenant. StatusEnum enumerator was also created that is used as the Status property of the Request class. Class has not been tested. [finishes #149087171]

--- HousingTenant.Business/HousingTenant.Business.Library/Models/Request.cs
+++ HousingTenant.Business/HousingTenant.Business.Library/Models/Request.cs
@@ -4,13 +4,27 @@ using System.Text;
 
 namespace HousingTenant.Business.Library.Models
 {
-  public class Request
-  {
-    public Person person { get; set; }
-    //public Apartment Apartment { get; set; }
-    public string Content { get; set; }
-    public string RequestType { get; set; }
-    public DateTime Time { get; set; }
-    public bool Status { get; set; }
-  }
+    /// <summary>
+    /// Represent a request for accommodation or supplies
+    /// </summary>
+    public abstract class ARequest
+    {
+        public bool Urgent { get; set; }
+        public Person Initiator { get; set; }
+        public DateTime DateSubmitted { get; set; }
+        public DateTime DateModified { get; set; }
+        public StatusEnum Status { get; set; }
+
+        /// <summary>
+        /// Generate a string representation of this class
+        /// </summary>
+        /// <returns>
+        /// String representation of this class instance
+        /// </returns>
+        public override string ToString()
+        {
+            return string.Format("From: {0}\nOn: {1}",
+               Initiator, DateSubmitted);
+        }
+    }
 }
