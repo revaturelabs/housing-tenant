432b15372771a9152a3469cea7d35b9b7f68e9bf
omar reid <omar.reid@verizon.net>
2017-07-18 19:49:47 -0400

    Created MoveRequest class. This class is derived from ARequest class and will be used by tenants to submit a request to move to another apartment. Class has not been tested. [finishes #149125695]

---
+++ HousingTenant.Business/HousingTenant.Business.Library/Models/MoveRequest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingTenant.Business.Library.Models
+{
+    public class MoveRequest : ARequest
+    {
+        public string Reason { get; set; }
+        public Address RequestedApartmentAddress { get; set; }
+
+        /// <summary>
+        /// Compares two Move Request for equality using the initiator
+        /// and the apartment address requested
+        /// </summary>
+        /// <param name="obj">The object this request is being compared with</param>
+        /// <returns>true if the initiators are the same and the address is the same,
+        /// false otherwise</returns>
+        public override bool Equals(object obj)
+        {
+            if(obj != null && obj.GetType() == GetType())
+            {
+                var newMoveRequest = obj as MoveRequest;
+
+                return newMoveRequest.Initiator.Equals(Initiator) &&
+                   newMoveRequest.RequestedApartmentAddress.Equals(RequestedApartmentAddress);
+            }
+             return false;
+        }
+
+        /// <summary>
+        /// Generate a unique number for each instance of this object
+        /// </summary>
+        /// <returns>a unique number to this object instance</returns>
+        public override int GetHashCode()
+        {
+            return Initiator.GetHashCode() + RequestedApartmentAddress.GetHashCode();
+        }
+
+        /// <summary>
+        /// Generate a string representation of this class
+        /// </summary>
+        /// <returns>
+        /// String representation of this class instance
+        /// </returns>
+        public override string ToString()
+        {
+            return string.Format("{0}\nReason: {1}",
+               base.ToString(), Reason);
+        }
+   }
+}
