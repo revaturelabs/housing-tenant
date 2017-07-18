6aa2a009ab35d82a525035802bd3f21adf941a93
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-18 11:38:45 -0400

    Removed any already existing models involving requests and created the RequestDAO model [#148795393]

---
+++ HousingTenant.Data/HousingTenant.Data.Service/Models/RequestDAO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingTenant.Data.Service.Models
+{
+    public class RequestDAO
+    {
+    }
+}
