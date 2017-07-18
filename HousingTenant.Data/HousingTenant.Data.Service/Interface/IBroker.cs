9260b0471f929c6f016f60bfcaca2efc114371ce
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-18 13:02:32 -0400

    Create IBroker interface [#149086383]

--- HousingTenant.Data/HousingTenant.Data.Service/Interface/IBroker.cs
+++ HousingTenant.Data/HousingTenant.Data.Service/Interface/IBroker.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace HousingTenant.Data.Service.Brokers
+namespace HousingTenant.Data.Service.Interface
 {
-    interface IDataBroker
+    public interface IBroker
     {
-      T Get<T>();
     }
 }
