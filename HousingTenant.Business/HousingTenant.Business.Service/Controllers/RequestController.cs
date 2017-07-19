d9a0f9babe6322ae5259ba6c8d464f7bfbdc0aec
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-18 22:58:04 -0400

    Created Post function for RequestController [#149112535]

--- HousingTenant.Business/HousingTenant.Business.Service/Controllers/RequestController.cs
+++ HousingTenant.Business/HousingTenant.Business.Service/Controllers/RequestController.cs
@@ -31,8 +31,9 @@ namespace HousingTenant.Business.Service.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public bool Post([FromBody]ARequest value)
         {
+            return rb.Create (value);
         }
 
         // PUT api/values/5
