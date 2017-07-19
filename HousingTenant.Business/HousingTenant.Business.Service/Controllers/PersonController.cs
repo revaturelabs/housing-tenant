b2c68e03df4fa9cc6ea087f0e039bb6ab99542e8
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-19 16:32:40 -0400

    Created Get function for PersonController [#148797753]

--- HousingTenant.Business/HousingTenant.Business.Service/Controllers/PersonController.cs
+++ HousingTenant.Business/HousingTenant.Business.Service/Controllers/PersonController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using HousingTenant.Business.Service.Brokers;
+using HousingTenant.Business.Library.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,19 +13,21 @@ namespace HousingTenant.Business.Service.Controllers
     [Route("api/[controller]")]
     public class PersonController : Controller
     {
+        private static PersonBroker pb = new PersonBroker ();
+
         // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Person> Get()
         {
-            return new string[] { "value1", "value2" };
+            return pb.Get ();
         }
 
-        // GET api/values/5
+        /*// GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
-        }
+        }*/
 
         // POST api/values
         [HttpPost]
@@ -31,16 +35,16 @@ namespace HousingTenant.Business.Service.Controllers
         {
         }
 
-        // PUT api/values/5
+        /*// PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
-        }
+        }*/
 
-        // DELETE api/values/5
+        /*// DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-        }
+        }*/
     }
 }
