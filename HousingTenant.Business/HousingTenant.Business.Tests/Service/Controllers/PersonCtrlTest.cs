65a384e41e658e0c109e6723b3981bcd1fdeba53
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-19 17:11:14 -0400

    Created Test class for Person Controller [#148797753]

---
+++ HousingTenant.Business/HousingTenant.Business.Tests/Service/Controllers/PersonCtrlTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using HousingTenant.Business.Service.Controllers;
+using HousingTenant.Business.Library.Models;
+
+namespace HousingTenant.Business.Tests.Service.Controllers
+{
+    public class PersonCtrlTest
+    {
+
+    }
+}
