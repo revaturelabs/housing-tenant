649f4e7df04675ff350ac4260e435f33914fe362
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-19 16:59:42 -0400

    Created PersonBrokerTest [#148797753]

---
+++ HousingTenant.Business/HousingTenant.Business.Tests/Service/Brokers/PersonBrokerTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingTenant.Business.Tests.Service.Brokers
+{
+    public class PersonBrokerTest
+    {
+    }
+}
