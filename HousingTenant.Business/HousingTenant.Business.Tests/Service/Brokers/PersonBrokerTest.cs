093371f5b478eb3031c412015a505c6f4be24c4b
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-19 17:06:32 -0400

    Created test for the Get function of the PersonBroker [#148797753]

--- HousingTenant.Business/HousingTenant.Business.Tests/Service/Brokers/PersonBrokerTest.cs
+++ HousingTenant.Business/HousingTenant.Business.Tests/Service/Brokers/PersonBrokerTest.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using HousingTenant.Business.Service.Brokers;
+using HousingTenant.Business.Library.Models;
+using NUnit.Framework;
 
 namespace HousingTenant.Business.Tests.Service.Brokers
 {
+    [TestFixture]
     public class PersonBrokerTest
     {
+        private static PersonBroker pb = new PersonBroker ();
+
+        [Test]
+        public void GetAllTest()
+        {
+            var actual = new List<Person> ();
+            var expected = 0;
+
+            actual = pb.Get ();
+
+            Assert.IsTrue (actual.Count > expected);
+        }
     }
 }
