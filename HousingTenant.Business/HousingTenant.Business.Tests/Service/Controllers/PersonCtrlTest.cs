5c4d0c35b5d5502829b63bc8af1f236db50794c8
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-19 17:20:10 -0400

    modified GetAllTest for PersonCtrlTest [#148797753]

--- HousingTenant.Business/HousingTenant.Business.Tests/Service/Controllers/PersonCtrlTest.cs
+++ HousingTenant.Business/HousingTenant.Business.Tests/Service/Controllers/PersonCtrlTest.cs
@@ -7,8 +7,20 @@ using HousingTenant.Business.Library.Models;
 
 namespace HousingTenant.Business.Tests.Service.Controllers
 {
+    [TestFixture]
     public class PersonCtrlTest
     {
+        [Test]
+        public void GetAllTest()
+        {
+            var expected = new List<Person> ();
+            var actual = 0;
 
+            var pc = new PersonController ();
+
+            expected = (pc.Get ()) as List<Person>;
+
+            Assert.IsTrue (expected.Count > actual);
+        }
     }
 }
