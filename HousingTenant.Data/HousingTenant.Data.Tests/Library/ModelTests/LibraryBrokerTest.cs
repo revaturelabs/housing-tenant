75166ed11044f67e2feae18984073cbc23ee310d
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-20 16:38:30 -0400

    Moved preexisting tests to a new location and created LibraryBrokerTest [#149260163]

---
+++ HousingTenant.Data/HousingTenant.Data.Tests/Library/ModelTests/LibraryBrokerTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HousingTenant.Data.Library.DataModels;
+
+namespace HousingTenant.Data.Tests.Library.ModelTests
+{
+    public class LibraryBrokerTest
+    {
+
+    }
+}
