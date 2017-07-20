6b146ab0b6c2b6b14649474d3e791fd4bdf6ea11
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-20 09:39:29 -0400

    Modified ServiceBroker class [#149154275]

--- HousingTenant.Data/HousingTenant.Data.Service/Models/ServiceBroker.cs
+++ HousingTenant.Data/HousingTenant.Data.Service/Models/ServiceBroker.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HousingTenant.Data.Service.Abstract;
+using HousingTenant.Data.Service.Interfaces;
 
 namespace HousingTenant.Data.Service.Models
 {
-    public class ServiceBroker
+    public class ServiceBroker<T> : ABroker<T> where T : IModel
     {
+        public override List<T> Get()
+        {
+            var list = new List<T> ();
+
+
+
+            return list;
+        }
+
+        public override bool Create(T obj)
+        {
+            return true;
+        }
     }
 }
