442a9cc6a58b440d683268ff15e48daa0645ef20
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-20 17:15:16 -0400

    Created GetBrokerTest to test the BrokerFactory class [#149260163]

---
+++ HousingTenant.Data/HousingTenant.Data.Tests/Library/BrokerFactoryTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using HousingTenant.Data.Library.Factory;
+
+namespace HousingTenant.Data.Tests.Library
+{
+    [TestFixture]
+    public class BrokerFactoryTest
+    {
+        [Test]
+        public void GetBrokerTest()
+        {
+            var actual = new BrokerFactory<Object> ().Create ();
+
+            Assert.IsNotNull (actual);
+        }
+    }
+}
