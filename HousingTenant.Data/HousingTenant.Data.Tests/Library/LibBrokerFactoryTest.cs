711386938cd546e8ee7c93991101d90360a687a0
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-20 17:24:59 -0400

    Renamed test class [#149260163]

--- HousingTenant.Data/HousingTenant.Data.Tests/Library/LibBrokerFactoryTest.cs
+++ HousingTenant.Data/HousingTenant.Data.Tests/Library/LibBrokerFactoryTest.cs
@@ -7,7 +7,7 @@ using HousingTenant.Data.Library.Factory;
 namespace HousingTenant.Data.Tests.Library
 {
     [TestFixture]
-    public class BrokerFactoryTest
+    public class LibBrokerFactoryTest
     {
         [Test]
         public void GetBrokerTest()
