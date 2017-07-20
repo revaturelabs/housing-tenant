85baf2a24177e4ea76ab650989a07b54357eb915
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-20 16:18:07 -0400

    Created IBroker class [#149260163]

---
+++ HousingTenant.Data/HousingTenant.Data.Library/Interfaces/IBroker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HousingTenant.Data.Library.Interfaces
+{
+    public interface IBroker<T>
+    {
+        List<T> Get();
+        bool Create(T obj);
+    }
+}
