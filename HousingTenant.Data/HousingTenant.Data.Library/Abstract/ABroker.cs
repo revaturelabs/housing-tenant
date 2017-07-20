33cad871acd394b0ec144cde5ac2bb828664e565
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-20 16:21:58 -0400

    Created ABroker class [#149260163]

---
+++ HousingTenant.Data/HousingTenant.Data.Library/Abstract/ABroker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HousingTenant.Data.Library.Interfaces;
+
+namespace HousingTenant.Data.Library.Abstract
+{
+    public abstract class ABroker<T> : IBroker<T>
+    {
+        public virtual bool Create(T obj)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public virtual List<T> Get()
+        {
+            throw new NotImplementedException ();
+        }
+    }
+}
