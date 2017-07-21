5c69af3f4a3c21b7a3e84b2e2b42ecf24615fb79
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-20 22:40:16 -0400

    Created mapperFactoryTest with blank test GetMapperTest [#149267837]

---
+++ HousingTenant.Data/HousingTenant.Data.Tests/Service/MapperFactoryTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using HousingTenant.Data.Service.Factory;
+
+namespace HousingTenant.Data.Tests.Service
+{
+    [TestFixture]
+    public class MapperFactoryTest
+    {
+        [Test]
+        public void GetMapperTest()
+        {
+
+        }
+    }
+}
