4b728b2b9c69d6e3a33f51827820ac6081b047a1
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-21 13:57:54 -0400

    Created PersonCtrlTest [#149307265]

---
+++ HousingTenant.Data/HousingTenant.Data.Tests/Service/Controllers/PersonCtrlTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace HousingTenant.Data.Tests.Service.Controllers
+{
+    [TestFixture]
+    public class PersonCtrlTest
+    {
+        
+    }
+}
