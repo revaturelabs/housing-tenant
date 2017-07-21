0103e55595db8366e733f8d8a225a653ce560b02
Julian Rojas <rojasmolinajulian@gmail.com>
2017-07-20 23:21:48 -0400

    view tells how many requests are for each type [delivers #149230009]

--- HousingTenant.Client/ngapp/apartment/service.ts
+++ HousingTenant.Client/ngapp/apartment/service.ts
@@ -1,5 +1,6 @@
 import {apartmentModule as am} from './module';
 
+
 var appartmentService = am.factory('aptFactory', ['$http', function($http){
    return{
       getApartment : function(scope,address){
@@ -24,6 +25,59 @@ var appartmentService = am.factory('aptFactory', ['$http', function($http){
                      lastname: 'Bruuuhhh'
                   }
                ];
+               scope.apartment.requests = [
+                  {
+                     soap : true,
+                     toiletPaper : true,
+                     paperTowels : true,
+                     dishSoap : true,
+                     trashBags : true,
+                     dishwasherDetergent : true,
+                     sponges : true,
+                     type : 1
+                  },
+                  {
+                     soap : true,
+                     toiletPaper : false,
+                     paperTowels : true,
+                     dishSoap : false,
+                     trashBags : true,
+                     dishwasherDetergent : false,
+                     sponges : true,
+                     type : 1
+                  },
+                  {
+                     soap : false,
+                     toiletPaper : false,
+                     paperTowels : false,
+                     dishSoap : false,
+                     trashBags : false,
+                     dishwasherDetergent : false,
+                     sponges : true,
+                     type : 1
+                  },
+                  {
+                     type : 2
+                  },
+                  {
+                     type : 3
+                  }
+               ];
+               scope.supplyReq = 0;
+               scope.maintenanceReq = 0;
+               scope.complaintReq = 0;
+               scope.apartment.requests.forEach(element => {
+                  if(element.type == 1){
+                     scope.supplyReq++;
+                     console.log(scope.maintenanceReq);
+                  }else if(element.type == 2){
+                     scope.maintenanceReq++;
+                     console.log(scope.maintenanceReq);
+                  }else{
+                     scope.complaintReq++;
+                     console.log(scope.complaintReq);
+                  };
+               });
                console.log(scope.apartment);
             }
          );
