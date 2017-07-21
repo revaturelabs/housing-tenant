94d87e993b57d71b63241e5d0c54946a0da4a37f
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-20 21:40:46 -0400

    Removed unneccessary files and lines of code [#149267837]

--- HousingTenant.Data/HousingTenant.Data.Service/Controllers/RequestController.cs
+++ HousingTenant.Data/HousingTenant.Data.Service/Controllers/RequestController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using HousingTenant.Data.Service.Brokers;
 using HousingTenant.Data.Service.Models;
 using HousingTenant.Data.Service.Interfaces;
 using HousingTenant.Data.Service.Factory;
@@ -15,7 +14,6 @@ namespace HousingTenant.Data.Service.Controllers
     [Route("api/[controller]")]
     public class RequestController : Controller
     {
-        private static RequestBroker rb = new RequestBroker ();
         private static IBroker<RequestDTO> srb = new BrokerFactory<RequestDTO> ().Create ();
 
         // GET: api/values
@@ -28,30 +26,11 @@ namespace HousingTenant.Data.Service.Controllers
             return list;
         }
 
-        /*// GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }*/
-
         // POST api/values
         [HttpPost]
         public bool Post([FromBody]RequestDTO value)
         {
             return srb.Create (value);
         }
-
-        /*// PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }*/
-
-       /* // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }*/
     }
 }
