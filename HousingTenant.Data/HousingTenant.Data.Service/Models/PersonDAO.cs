93c29d23416b76e467b187dd008e06eb645928bf
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-21 11:58:38 -0400

    Created personDAO model [#149307265]

---
+++ HousingTenant.Data/HousingTenant.Data.Service/Models/PersonDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HousingTenant.Data.Service.Interfaces;
+
+namespace HousingTenant.Data.Service.Models
+{
+    public class PersonDAO : IModel
+    {
+        public int Gender { get; set; }
+        public bool HasCar { get; set; }
+        public string FirstName { set; get; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime ReportDate { get; set; }
+    }
+}
