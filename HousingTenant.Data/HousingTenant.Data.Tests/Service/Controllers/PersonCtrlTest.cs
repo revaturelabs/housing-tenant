dc28fd6b0f2ad77bdb65c152df316e83ad4efd44
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-21 15:55:17 -0400

    Added tests to PersonCtrlTest [#149307265]

--- HousingTenant.Data/HousingTenant.Data.Tests/Service/Controllers/PersonCtrlTest.cs
+++ HousingTenant.Data/HousingTenant.Data.Tests/Service/Controllers/PersonCtrlTest.cs
@@ -2,12 +2,43 @@
 using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
+using HousingTenant.Data.Service.Controllers;
+using HousingTenant.Data.Service.Models;
 
 namespace HousingTenant.Data.Tests.Service.Controllers
 {
     [TestFixture]
     public class PersonCtrlTest
     {
-        
+        private PersonController pc;
+
+        public PersonCtrlTest()
+        {
+            pc = new PersonController ();
+        }
+
+        [Test]
+        public void GetAllTest()
+        {
+            var expected = pc.Get ();
+            var actual = 0;
+
+            Assert.IsTrue (expected.Count > actual);
+        }
+
+        [Test]
+        public void GetSpecificTest()
+        {
+            var expected = pc.Get ("Jason");
+            var actual = 0;
+
+            Assert.IsTrue (expected.Count > actual);
+        }
+
+        [Test]
+        public void PostTest()
+        {
+            Assert.IsTrue (pc.Post (new PersonDAO { FirstName = "some"}));
+        }
     }
 }
