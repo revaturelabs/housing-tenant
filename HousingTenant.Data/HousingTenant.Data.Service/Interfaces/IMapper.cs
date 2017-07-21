2ad06ab91ed952a77901335c2dc85fb303f98d57
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-20 22:20:22 -0400

    Created ServiceMapper and IMapper [#149267837]

---
+++ HousingTenant.Data/HousingTenant.Data.Service/Interfaces/IMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HousingTenant.Data.Service.Interfaces
+{
+    public interface IMapper<T,U>
+    {
+        U MapToU(T obj);
+        T MapToT(U obj);
+    }
+}
---
+++ HousingTenant.Data/HousingTenant.Data.Service/Models/ServiceMapper.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HousingTenant.Data.Service.Interfaces;
+
+namespace HousingTenant.Data.Service.Models
+{
+    public class ServiceMapper<T,U> : IMapper<T,U> where T : new() where U : new()
+    {
+        private MapperConfiguration tTou = new MapperConfiguration (p => p.CreateMap<T, U> ());
+        private MapperConfiguration uTot = new MapperConfiguration (p => p.CreateMap<U, T> ());
+
+        public U MapToU(T obj)
+        {
+            return tTou.CreateMapper ().Map<U> (obj);
+        }
+
+        public T MapToT(U obj)
+        {
+            return tTou.CreateMapper ().Map<T> (obj);
+        }
+    }
+}
