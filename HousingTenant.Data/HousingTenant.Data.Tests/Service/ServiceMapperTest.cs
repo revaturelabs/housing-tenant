f0a2216169938d52627464c421b4164e0bbbb924
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-20 22:48:01 -0400

    Created ServiceMapperTest with two tests [#149267837]

---
+++ HousingTenant.Data/HousingTenant.Data.Tests/Service/ServiceMapperTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using HousingTenant.Data.Service.Models;
+
+namespace HousingTenant.Data.Tests.Service
+{
+    [TestFixture]
+    public class ServiceMapperTest
+    {
+        [Test]
+        public void MapToUTest()
+        {
+
+        }
+
+        [Test]
+        public void MapToTTest()
+        {
+
+        }
+    }
+}
+}
