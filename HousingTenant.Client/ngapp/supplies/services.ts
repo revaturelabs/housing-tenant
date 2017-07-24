cd8f607e700cdeed3078912566f2fb34074f2c8e
Julian Rojas <rojasmolinajulian@gmail.com>
2017-07-24 10:50:58 -0400

    Updated Supply Service  [#149408959]

--- HousingTenant.Client/ngapp/supplies/services.ts
+++ HousingTenant.Client/ngapp/supplies/services.ts
@@ -3,7 +3,7 @@ import {supplyModule as sm} from './module';
 var supplyService = sm.factory('supplyRequestListSvc', ['$http', function($http){
    return {
       getRequestList: function(aptidstring, scope){
-         $http.get('http://localhost:5000/api/request/', { params: aptidstring } ).then(
+         $http.get('http://housingtenantbusiness.azurewebsites.net/api/request', { params: aptidstring } ).then(
             function(res){
                console.log(res.data); 
                   scope.reqList = {};
@@ -21,10 +21,10 @@ var supplyService = sm.factory('supplyRequestListSvc', ['$http', function($http)
             console.log(request);
             $http({
                   method: 'POST',
-                  url: 'http://localhost:5000/api/request/',
+                  url: 'http://housingtenantbusiness.azurewebsites.net/api/request',
                   withCredentials: true,
                   headers: {
-                        'Access-Control-Allow-Origin': 'http://localhost:5000',
+                        'Access-Control-Allow-Origin': 'http://housingtenantbusiness.azurewebsites.net',
                         'Content-Type': 'application/json',
                         'Access-Control-Allow-Credentials' : 'true',
                         'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE'
