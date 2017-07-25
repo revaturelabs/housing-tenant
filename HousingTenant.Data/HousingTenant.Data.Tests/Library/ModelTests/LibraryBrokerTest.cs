ca413aa3ce204b39eb847ef9f5359567b9dc89e6
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-25 15:17:45 -0400

    Removed unneccessary files [#149497487]

--- HousingTenant.Data/HousingTenant.Data.Tests/Library/ModelTests/LibraryBrokerTest.cs
+++ HousingTenant.Data/HousingTenant.Data.Tests/Library/ModelTests/LibraryBrokerTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using HousingTenant.Data.Library.DataModels;
+using HousingTenant.Data.Library.AzModels;
 using NUnit.Framework;
 
 namespace HousingTenant.Data.Tests.Library.ModelTests
@@ -9,10 +10,17 @@ namespace HousingTenant.Data.Tests.Library.ModelTests
     [TestFixture]
     public class LibraryBrokerTest
     {
+        private HousingTenantDBContext _Context;
+
+        public LibraryBrokerTest(HousingTenantDBContext context)
+        {
+            _Context = context;
+        }
+
         [Test]
         public void GetAllTest()
         {
-            var lb = new LibraryBroker<Object> ();
+            var lb = new LibraryBroker<Address> (_Context);
             var actual = lb.GetAll ();
             var expected = 0;
 
