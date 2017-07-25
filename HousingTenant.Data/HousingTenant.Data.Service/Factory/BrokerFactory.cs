b1d3287ea4e55070525234d1b7aca75ba312bd4d
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-25 13:09:39 -0400

    Modified multiple classes to adjust for changes to LibraryBroker [#149497487]

--- HousingTenant.Data/HousingTenant.Data.Service/Factory/BrokerFactory.cs
+++ HousingTenant.Data/HousingTenant.Data.Service/Factory/BrokerFactory.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using HousingTenant.Data.Service.Models;
 using HousingTenant.Data.Service.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace HousingTenant.Data.Service.Factory
 {
-    public class BrokerFactory<T,U> where T : IModel, new() where U : new()
+    public class BrokerFactory<T,U> where T : IModel, new() where U : class, new()
     {
-        public ServiceBroker<T,U> Create()
+        public ServiceBroker<T,U> Create(DbContext context)
         {
-            return new ServiceBroker<T,U>();
+            return new ServiceBroker<T,U>(context);
         }
     }
 }
--- HousingTenant.Data/HousingTenant.Data.Service/Models/ServiceBroker.cs
+++ HousingTenant.Data/HousingTenant.Data.Service/Models/ServiceBroker.cs
@@ -5,13 +5,20 @@ using System.Threading.Tasks;
 using HousingTenant.Data.Service.Abstract;
 using HousingTenant.Data.Service.Interfaces;
 using HousingTenant.Data.Service.Factory;
+using Microsoft.EntityFrameworkCore;
 
 namespace HousingTenant.Data.Service.Models
 {
-    public class ServiceBroker<T,U> : ABroker<T> where T : IModel, new() where U : new()
+    public class ServiceBroker<T,U> : ABroker<T> where T : IModel, new() where U : class, new()
     {
-        private Library.Interfaces.IBroker<U> lb = new Library.Factory.BrokerFactory<U> ().Create();
-        private IMapper<T, U> sm = new MapperFactory<T, U> ().Create ();
+        private Library.Interfaces.IBroker<U> lb;
+        private IMapper<T, U> sm;
+
+        public ServiceBroker(DbContext context)
+        {
+            lb = new Library.Factory.BrokerFactory<U> ().CreateLibBroker (context);
+            sm = new MapperFactory<T, U> ().Create ();
+        }
 
         public override List<T> GetAll()
         {
