fa97ac33e0a11b9dbafd7095acab4fd358d5541b
Julian Rojas <rojasmolinajulian@gmail.com>
2017-07-25 14:44:49 -0400

    added nuget package [#149513569]

--- HousingTenant.Business/HousingTenant.Business.Service/Startup.cs
+++ HousingTenant.Business/HousingTenant.Business.Service/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+
 namespace HousingTenant.Business.Service
 {
     public class Startup
@@ -27,9 +28,11 @@ namespace HousingTenant.Business.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-      // Add framework services.
-      services.AddCors(o => o.AddPolicy("default", b => b.AllowAnyOrigin().AllowAnyHeader()));
-      services.AddMvc();
+            //#149513569
+            // Add framework services.
+
+            //services.AddCors(o => o.AddPolicy("default", b => b.AllowAnyOrigin().AllowAnyHeader()));
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
