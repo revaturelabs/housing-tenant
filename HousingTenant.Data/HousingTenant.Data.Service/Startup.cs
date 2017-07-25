0d925cb61490c53c670cdd3fc1e56180ad16fb79
Kevin Durand <kevincdurand1@me.com>
2017-07-25 13:28:23 -0400

    Added AllowAnyHeaders() in Cors. [finishes #149495203]

--- HousingTenant.Data/HousingTenant.Data.Service/Startup.cs
+++ HousingTenant.Data/HousingTenant.Data.Service/Startup.cs
@@ -32,7 +32,7 @@ namespace HousingTenant.Data.Service
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddCors(o => o.AddPolicy("default", b => b.AllowAnyOrigin()));
+            services.AddCors(o => o.AddPolicy("default", b => b.AllowAnyOrigin().AllowAnyHeader()));
             services.AddSingleton(Configuration);
             services.AddDbContext<HousingTenantDBContext>();
             services.AddMvc();
