ae3013983410c6ba83eb4c346976aa6cd0ad7770
Donald Hokenson <hokensondonald@yahoo.com>
2017-07-26 02:36:07 -0400

    Modified tests in RequestCtrlTest [#149520139]

--- HousingTenant.Data/HousingTenant.Data.Tests/Service/Controllers/RequestCtrlTest.cs
+++ HousingTenant.Data/HousingTenant.Data.Tests/Service/Controllers/RequestCtrlTest.cs
@@ -4,20 +4,22 @@ using System.Text;
 using NUnit.Framework;
 using HousingTenant.Data.Service.Controllers;
 using HousingTenant.Data.Service.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace HousingTenant.Data.Tests.Service.Controllers
 {
     [TestFixture]
     public class RequestCtrlTest
     {
-        [Test]
-        public void GetAllTest()
-        {
-            var ctrl = new RequestController ();
-            var actual = new List<RequestDAO>();
-            var expected = 0;
+        HttpClient client = new HttpClient { BaseAddress = new Uri ("https://housingtenantdata.azurewebsites.net/api/") };
 
-            actual = ctrl.Get ();
+        [Test]
+        public async void GetAllTest()
+        {
+            var requests = await client.GetAsync ("request", HttpCompletionOption.ResponseContentRead);
+            var actual = JsonConvert.DeserializeObject<List<RequestDAO>> (requests.Content.ReadAsStringAsync ().Result);
+            var expected = 0;
 
             Assert.IsTrue (actual.Count > expected);
         }
