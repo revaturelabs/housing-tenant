7974d47bf74b1a83e80bbbe5edb1cb26b723ae02
omar reid <omar.reid@verizon.net>
2017-07-28 11:45:29 -0400

    Started implementation of validation. [#149738691]

--- HousingTenant.Business/HousingTenant.Business.Service/Controllers/ApartmentController.cs
+++ HousingTenant.Business/HousingTenant.Business.Service/Controllers/ApartmentController.cs
@@ -52,31 +52,7 @@ namespace HousingTenant.Business.Service.Controllers
 
             return ServiceMapper.MapToApartmentDTO((Apartment)apartment);
             //  return ServiceMapper.MapToApartmentDTO(_ServiceManager.GetApartment(id));
-      }
-
-        [HttpGet]
-        [Route("address")]
-        public ApartmentDTO Get([FromQuery]Address address)
-        //public async Task<IApartment> Get([FromQuery]Address address)
-        {
-         //var apartmentUrl = string.Format("apartment/{0}", address);
-         //var aRequest = await client.GetAsync(apartmentUrl, HttpCompletionOption.ResponseContentRead);
-         //var emptyApartment = JsonConvert.DeserializeObject<Apartment>(aRequest.Content.ReadAsStringAsync().Result);
-
-         //var personUrl = string.Format("person/{0}", address);
-         //var pRequest = await client.GetAsync(personUrl, HttpCompletionOption.ResponseContentRead);
-         //var persons = JsonConvert.DeserializeObject<List<Person>>(pRequest.Content.ReadAsStringAsync().Result);
-
-         //var requestUrl = string.Format("request/{0}", address);
-         //var rRequest = await client.GetAsync(requestUrl, HttpCompletionOption.ResponseContentRead);
-         //var requests = JsonConvert.DeserializeObject<List<RequestDTO>>(rRequest.Content.ReadAsStringAsync().Result);
-
-         //var apartment = _LibraryManager.PackApartment(emptyApartment, ServiceMapper.MapToIPersonList(persons), ServiceMapper.MapToARequestList(requests));
-
-         //return apartment as Apartment;
-
-         return ServiceMapper.MapToApartmentDTO(_ServiceManager.GetApartment(address));
-      }
+        }
 
         // POST api/values
         [HttpPost]
